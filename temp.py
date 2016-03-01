727b4bc9fc4490a1f9de597e9a2263aa42d4a120
ireneycho <ireneychoster@gmail.com>
2016-02-29 17:31:21 -0800

    initialize
    initialize files

---
+++ temp.py
@@ -0,0 +1,29 @@
+from kivy.app import App
+#kivy.require("1.9.2")
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.floatlayout import FloatLayout
+#from kivy.animation import Animation, AnimationTransition
+#from kivy.uix.widget import Widget
+
+
+class HomeScreen(Screen):
+    pass
+class OptionScreen(Screen):
+    pass
+class ScreenManagement(ScreenManager):
+    pass
+#class CustomLayout(FloatLayout):
+    #pass
+#class SmileAnimate(Animation):
+    #pass
+
+screens = Builder.load_file("temp.kv")
+
+class MainApp(App):
+    def build(self):
+        return screens
+
+#if __name__ == '__main__':
+MainApp().run()
+                       
